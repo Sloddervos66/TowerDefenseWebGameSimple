b23bc999f1cfa55cda1e6b116c220e6cd85245a4
Sloddervos66 <sloddervos66@gmail.com>
2024-03-31 20:06:15 +0200

    audio

---
+++ audioPicker.js
@@ -0,0 +1,33 @@
+const audio = document.getElementById('audioPlayer');
+const source = document.getElementById('sourceMusic');
+
+const music = [
+    'sounds/Beast Ganon Battle - The Legend of Zelda Twilight Princess.mp3',
+    'sounds/Boss Defeated - The Legend of Zelda Twilight Princess.mp3',
+    'sounds/Death Mountain - The Legend of Zelda Twilight Princess.mp3',
+    'sounds/Faron Woods - The Legend of Zelda Twilight Princess.mp3',
+    'sounds/Ganondorf - The Legend of Zelda Twilight Princess.mp3',
+    'sounds/Hidden Skill Training - The Legend of Zelda Twilight Princess.mp3',
+    'sounds/Hidden Village - The Legend of Zelda Twilight Princess.mp3',
+    'sounds/Horseback Ganondorf Fight - The Legend of Zelda Twilight Princess.mp3',
+    'sounds/Lake Hylia - The Legend of Zelda Twilight Princess.mp3',
+    'sounds/Midna\'s Lament - The Legend of Zelda Twilight Princess.mp3',
+    'sounds/Puppet Zelda - The Legend of Zelda Twilight Princess.mp3',
+    'sounds/Temple of Time - The Legend of Zelda Twilight Princess.mp3',
+    'sounds/Zant Battle - The Legend of Zelda Twilight Princess.mp3'
+];
+
+let currentTrackIndex = 0;
+
+function playNextTrack() {
+    currentTrackIndex = (currentTrackIndex + 1) % music.length;
+    source.src = music[currentTrackIndex];
+    audio.load(); // This will load the new audio source
+    audio.play(); // This will start playing the new audio
+    console.log(currentTrackIndex);
+}
+
+source.src = music[currentTrackIndex]; // Set initial source
+audio.load(); // Load the initial audio source
+
+audio.addEventListener('ended', playNextTrack);
